cdfb8cde54d3f076698e6b15f3caca2ad292521f
ekqld12 <65323819+ekqld12@users.noreply.github.com>
2020-07-30 11:42:06 +0900

    최종 환성 코드

---
+++ src/routes/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './About.css';
+
+
+
+function About(props){
+    console.log(props)
+    return (
+        <div className="about__container">
+            <span>About this page...........</span>
+            <span>About this page...........</span>
+            <span>About this page...........</span>
+        </div>
+    )
+}
+
+
+export default About;
